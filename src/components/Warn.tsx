7ad9e44f9c98997baff040d39a2846dea4831b85
Mister McGee <141380054+mister-mcgee@users.noreply.github.com>
2024-01-20 13:16:12 -0700

    py-getting-started

--- src/components/Warn.tsx
+++ src/components/Warn.tsx
@@ -1,7 +1,6 @@
 import { Warning } from "@phosphor-icons/react";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./ui/accordion";
-
 import { Collapsible, CollapsibleTrigger, CollapsibleContent } from "./ui/collapsible";
+import { useState } from "react";
 
 
 interface Props extends React.PropsWithChildren {
@@ -9,11 +8,25 @@ interface Props extends React.PropsWithChildren {
 }
 
 export function Warn({ warn, children }: Props) {
+  const [open, setOpen] = useState(false)
+
+  function onMouseOver (me: React.MouseEvent) {
+    setOpen(true )
+  }
+
+  function onMouseOut(me: React.MouseEvent) {
+    setOpen(false)
+  }
+
   return (
-    <div className="border border-primary-foreground p-4 rounded bg-secondary">
-      <Collapsible>
+    <div
+      onMouseOver={onMouseOver}
+      onMouseOut ={onMouseOut }
+      className="border border-primary-foreground p-4 rounded bg-secondary"
+    >
+      <Collapsible open={open}>
           <CollapsibleTrigger>
-            <span className="space-x-4 hover:underline">
+            <span className="space-x-4">
               <Warning className="inline"/><span>{ warn }</span>
             </span>
           </CollapsibleTrigger>
