bb0da2f277629dc457b45716f58335436b19e89d
shawn-mcgee <s.mcgeek07@gmail.com>
2025-04-22 09:33:45 -0600

    Bun + Astro

--- public/example/program.w
+++
@@ -1,4 +0,0 @@
-factorial(n)
-  :factorial(n - 1) * n if n > 1 else 1
-
-print(factorial(5))
--- src/app/idea/[...slug]/view-fold.tsx
+++
@@ -1,5 +0,0 @@
-import NavView from "@/components/nav/nav-view"
-
-export default function FoldView({ slug }: { slug: Array<string> }) {
-  return NavView({ root: `./idea/${slug.join("/")}` })
-}
