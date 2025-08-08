4d97241d8cfa3286cea910c941f79c6c1e1295a1
shawn-mcgee <s.mcgeek07@gmail.com>
2025-08-08 15:55:01 -0600

    syncing changes

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'
+  ],
+  theme: {},
+  plugins: [],
+} satisfies Config
