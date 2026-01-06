ad875d68c6962ee655b029c95f6b842aacd0149f
shawn-mcgee <s.mcgeek07@gmail.com>
2026-01-05 18:49:38 -0700

    Working on mobile layout

--- src/ui/components/learn/lesson/Tags.tsx
+++ src/ui/components/learn/lesson/Tags.tsx
@@ -2,6 +2,6 @@ import Tag from "./Tag";
 
 export default function Tags({ tags }: { tags: Array<string> | undefined }) {
   return <div className="w-fit flex gap-1 flex-wrap">
-    {tags?.map((tag, i) => <Tag key={i} tag={tag}/>)}
+    {tags?.map((tag) => <Tag key={tag} tag={tag}/>)}
   </div>
 }
